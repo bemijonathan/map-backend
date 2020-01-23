50a127e6abc1490a975105b317632161e81d105c
bemijonathan <bemijonathan@gmail.com>
2020-01-23 04:54:44 +0100

    cors

--- index.js
+++ index.js
@@ -8,6 +8,15 @@ const server = http.createServer(app);
 const port = process.env.PORT || 3000
 const { createLink, UpdateLocation, killLink, AddNewUser, RemoveUser}  = require('./user')
 
+
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
+
 const io = socket(server) 
 
 io.on('connection', function(socket){
